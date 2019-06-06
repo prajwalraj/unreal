d000d508e899e5dc3e80313e68c240458f491e4a
Prajwal R <prajuu1996@gmail.com>
2019-06-07 04:27:31 +0530

    Moved scripts to unreal.js from index.html

--- unrealjourney/js/unreal.js
+++ unrealjourney/js/unreal.js
@@ -15,4 +15,23 @@ function carousel() {
 }
 
 
+// Change style of navbar on scroll
+window.onscroll = function() {myNavigation()};
+function myNavigation() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "unreal-bar" + "unreal-card" + "unreal-animate-top" + "unreal-white";
+    } else {
+        navbar.className = navbar.className.replace("unreal-card unreal-animate-top unreal-white", "");
+    }
+}
 
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("unreal-show") == -1) {
+        x.className += "unreal-show";
+    } else {
+        x.className = x.className.replace("unreal-show", "");
+    }
+}
