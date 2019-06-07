0ebd292a1636657913e35d0dae78c352f01b7238
Prajwal R <prajuu1996@gmail.com>
2019-06-08 00:11:10 +0530

    Bug fixes and used original w3css file

--- unrealjourney/js/unreal.js
+++ unrealjourney/js/unreal.js
@@ -1,37 +1,36 @@
-// Automatic Slideshow - change image every 4 seconds
-var myIndex = 0;
-carousel();
+// // Automatic Slideshow - change image every 4 seconds
+// var myIndex = 0;
+// carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 4000);    
-}
-
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 4000);    
+// }
 
 // Change style of navbar on scroll
-window.onscroll = function() {myNavigation()};
-function myNavigation() {
+window.onscroll = function() {myFunction()};
+function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "unreal-bar" + "unreal-card" + "unreal-animate-top" + "unreal-white";
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
-        navbar.className = navbar.className.replace("unreal-card unreal-animate-top unreal-white", "");
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
     }
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
     var x = document.getElementById("navDemo");
-    if (x.className.indexOf("unreal-show") == -1) {
-        x.className += "unreal-show";
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
     } else {
-        x.className = x.className.replace("unreal-show", "");
+        x.className = x.className.replace(" w3-show", "");
     }
 }
